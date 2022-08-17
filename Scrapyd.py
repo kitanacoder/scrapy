10966c75572f0596955b9bfc68dda7a157b031d0
kitanacoder <111438901+kitanacoder@users.noreply.github.com>
2022-08-17 16:32:49 +0300

    Add files via upload
    pip install virtualenv- настройка виртуального окружения
    pip install beautifulsoup4
    pip install requests

---
+++ Scrapyd.py
@@ -0,0 +1,20 @@
+#import html5lib
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+def skrapper():
+	print('---- start Skrapper ----')
+	html = urlopen("https://dveri.zp.ua")
+	bsObj = BeautifulSoup(html.read(), 'html.parser')
+	#bsObj = BeautifulSoup('features="html5lib"')
+	#nameList = bsObj.findAll("span", {"class":"green", "class":"red"})
+	nameList = bsObj.findAll()
+	for name in nameList:
+		print(name.get_text())
+	print('---- end Skrapper ----')
+
+
+if __name__ == "__main__":
+	skrapper()
+	print('---- hellow Im Works ----')
